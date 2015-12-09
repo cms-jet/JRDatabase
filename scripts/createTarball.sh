5664336da313b0d6a37a2f16bc147eb3b34d6abe
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-12-09 15:33:11 +0100

    First 2015 JER, Summer15_25nsV6

---
+++ scripts/createTarball.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# Create a tarball for a list of JER text files
+
+CORRECTIONS="MC_PtResolution DATAMCSF"
+
+P=$1
+
+if [ ! -d "$P" ]; then
+    echo "$P does not exists"
+    exit 1
+fi
+
+ERA=`basename $P`
+
+echo "Creating tarball for ${ERA}..."
+
+pushd $P &> /dev/null
+
+TARBALL=${ERA}.tar.gz
+
+CMD=""
+for C in $CORRECTIONS; do
+    CMD=${ERA}_${C}*" $CMD"
+done
+
+tar -hzcf ${TARBALL} ${CMD}
+
+popd &> /dev/null
+
+echo "Done. File saved as ${P}/${TARBALL}"
